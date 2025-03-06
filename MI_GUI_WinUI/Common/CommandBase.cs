05cf751dd08ff3a029a7e94c9cdd00ff5280f416
Yuma Noguchi <yuma.noguchi.22@ucl.ac.uk>
2025-03-06 23:35:40 +0000

    SD integrated

---
+++ MI_GUI_WinUI/Common/CommandBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace MI_GUI_WinUI.Common
+{
+    public class CommandBase<T> : ICommand where T : class
+    {
+        private readonly Action<T?> _execute;
+        private readonly Predicate<T?> _canExecute;
+
+        public CommandBase(Action<T?> execute, Predicate<T?> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        // Replace CommandManager with direct event
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke(parameter as T) ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute.Invoke(parameter as T);
+        }
+
+        public void RaiseCanExecute()
+        {
+            // Invoke the event directly
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
--- MI_GUI_WinUI/Pages/IconStudioPage.xaml.cs
+++ MI_GUI_WinUI/Pages/IconStudioPage.xaml.cs
@@ -1,12 +1,48 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
+using MI_GUI_WinUI.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MI_GUI_WinUI.Pages
 {
     public sealed partial class IconStudioPage : Page
     {
+        private readonly IconStudioViewModel ViewModel;
+
         public IconStudioPage()
         {
             this.InitializeComponent();
+            ViewModel = App.Current.Services.GetService<IconStudioViewModel>();
+            DataContext = ViewModel;
+            this.Loaded += IconStudioPage_Loaded;
+            this.Unloaded += Page_Unloaded;
+        }
+
+        private async void IconStudioPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //if (PulseStoryboard != null)
+            //{
+            //    PulseStoryboard.Begin();
+            //}
+
+            //// Ensure we have valid ViewModel and XamlRoot
+            //if (ViewModel != null)
+            //{
+            //    ViewModel.XamlRoot = this.XamlRoot;
+            //    await ViewModel.InitializeAsync();
+            //}
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            //if (PulseStoryboard != null)
+            //{
+            //    PulseStoryboard.Stop();
+            //}
+
+            //ViewModel?.Cleanup();
+
         }
     }
 }
